e3106c3729b32d540a0eb2f287c79f9277577df8
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-11-23 00:54:40 -0300

    Agrego error

--- src/components/Bienvenida.js
+++ src/components/Bienvenida.js
@@ -3,7 +3,7 @@ import "./Bienvenida.css"
 export const Bienvenida = () => {
 
     return (
-        <div className="container">
+        <div className="container"
             <h1 className="h1">Hola te estabamos esperando!</h1>
             <h2 className="h2">Aqui tienes los ultimos datos de tus pacientes</h2>
         </div>
